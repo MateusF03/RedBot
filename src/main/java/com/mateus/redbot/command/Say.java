3c44ab26ce479386057214c70198f74cd02197a8
BuildTools <unconfigured@null.spigotmc.org>
2020-01-14 12:18:33 -0300

    Fixing the Say command

--- src/main/java/com/mateus/redbot/command/Say.java
+++ src/main/java/com/mateus/redbot/command/Say.java
@@ -2,9 +2,7 @@ package com.mateus.redbot.command;
 
 import com.mateus.redbot.core.command.Command;
 import com.mateus.redbot.core.command.CommandCategory;
-import com.mateus.redbot.core.data.DataManager;
 import com.mateus.redbot.core.permissions.UserPermission;
-import com.mateus.redbot.utils.BotUtils;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 
@@ -14,16 +12,5 @@ public class Say {
         String text = String.join(" ", args);
         TextChannel textChannel = event.getChannel();
         textChannel.sendMessage(text).queue();
-        if (BotUtils.isOwner(event.getAuthor())) {
-            event.getJDA().getUsers().forEach(u -> {
-                if(!DataManager.getInstance().containsUserMoney(u)) {
-                    DataManager.getInstance().addUserMoney(u, (long) 100);
-                }
-                if(!DataManager.getInstance().containsUserGuildPermission(u, event.getGuild())) {
-                    UserPermission userPermission = event.getGuild().getOwnerId().equals(u.getId()) ? UserPermission.ADMIN : UserPermission.BASE;
-                    DataManager.getInstance().addUserPermission(userPermission, u, event.getGuild());
-                }
-            });
-        }
     }
 }
